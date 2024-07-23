9a8c534751d5e86ae1990a16aee48a59c20cfc21
Sandun Tharuka <sandun.suntws@gmail.com>
2024-07-23 12:14:38 +0530

    add validation pipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT');
