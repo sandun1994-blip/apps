d6629d87506e68fede5e21740ea070441e254055
Sandun Tharuka <sandun.suntws@gmail.com>
2024-07-23 12:15:12 +0530

    validate body

---
+++ api/src/customer/dtos/customer.dto.ts
@@ -0,0 +1,26 @@
+import { IsString, IsEmail, IsOptional } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+}
+
+export class UpdateCustomerDto {
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+}
