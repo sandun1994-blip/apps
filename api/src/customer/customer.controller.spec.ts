fc9099f5bd239fdd3779c4a532fd857333486f21
Sandun Tharuka <sandun.suntws@gmail.com>
2024-07-23 12:37:56 +0530

    add test cases

--- api/src/customer/customer.controller.spec.ts
+++ api/src/customer/customer.controller.spec.ts
@@ -1,18 +1,116 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
+import { CreateCustomerDto, UpdateCustomerDto } from './dtos/customer.dto';
+import { Customer } from '@prisma/client';
 
 describe('CustomerController', () => {
   let controller: CustomerController;
+  let service: CustomerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomerController],
+      providers: [
+        {
+          provide: CustomerService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CustomerController>(CustomerController);
+    service = module.get<CustomerService>(CustomerService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a customer', async () => {
+      const createCustomerDto: CreateCustomerDto = {
+        firstName: 'sandun',
+        lastName: 'tharuka',
+        email: 'sandun@gmail.com',
+      };
+      const result: Customer = {
+        id: 'id_one',
+        firstName: 'sandun',
+        lastName: 'tharuka',
+        email: 'sandun@gmail.com',
+      };
+      jest.spyOn(service, 'create').mockResolvedValue(result);
+
+      expect(await controller.create(createCustomerDto)).toBe(result);
+    });
+  });
+  describe('findAll', () => {
+    it('should return an array of customers', async () => {
+      const result: Customer[] = [
+        {
+          id: 'id_one',
+          firstName: 'sandun',
+          lastName: 'tharuka',
+          email: 'sandun@gmail.com',
+        },
+      ];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single customer', async () => {
+      const result: Customer = {
+        id: 'id_one',
+        firstName: 'sandun',
+        lastName: 'tharuka',
+        email: 'sandun@gmail.com',
+      };
+      jest.spyOn(service, 'findOne').mockResolvedValue(result);
+
+      expect(await controller.findOne('1')).toBe(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a customer', async () => {
+      const updateCustomerDto: UpdateCustomerDto = {
+        firstName: 'sandun',
+        lastName: 'senanayaka',
+        email: 'sandun@gmail.com',
+      };
+      const result: Customer = {
+        id: 'id_one',
+        firstName: 'sandun',
+        lastName: 'senanayaka',
+        email: 'sandun@gmail.com',
+      };
+      jest.spyOn(service, 'update').mockResolvedValue(result);
+
+      expect(await controller.update('id_one', updateCustomerDto)).toBe(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a customer', async () => {
+      const result: Customer = {
+        id: 'id_my',
+        firstName: 'sandun',
+        lastName: 'tharuka',
+        email: 'sandun@gmail.com',
+      };
+      jest.spyOn(service, 'remove').mockResolvedValue(result);
+
+      expect(await controller.remove('id_my')).toBe(result);
+    });
   });
 });
--- api/src/customer/customer.service.spec.ts
+++ api/src/customer/customer.service.spec.ts
@@ -1,18 +1,112 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Customer } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { CustomerService } from './customer.service';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('CustomerService', () => {
   let service: CustomerService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomerService],
+      providers: [
+        CustomerService,
+        {
+          provide: PrismaService,
+          useValue: {
+            customer: {
+              findMany: jest.fn(),
+              create: jest.fn(),
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CustomerService>(CustomerService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a customer', async () => {
+      const customer: Customer = {
+        id: 'id_one',
+        firstName: 'sandun',
+        lastName: 'Tharuka',
+        email: 'sandun@gmail.com',
+      };
+      jest.spyOn(prismaService.customer, 'create').mockResolvedValue(customer);
+
+      expect(await service.create(customer)).toBe(customer);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of customers', async () => {
+      const customers: Customer[] = [
+        {
+          id: 'id_one',
+          firstName: 'sandun',
+          lastName: 'Tharuka',
+          email: 'sandun@gmail.com',
+        },
+      ];
+      jest
+        .spyOn(prismaService.customer, 'findMany')
+        .mockResolvedValue(customers);
+
+      expect(await service.findAll()).toBe(customers);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single customer', async () => {
+      const customer: Customer = {
+        id: 'id_one',
+        firstName: 'sandun',
+        lastName: 'Tharuka',
+        email: 'sandun@gmail.com',
+      };
+      jest
+        .spyOn(prismaService.customer, 'findUnique')
+        .mockResolvedValue(customer);
+
+      expect(await service.findOne('id_one')).toBe(customer);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a customer', async () => {
+      const customer: Customer = {
+        id: 'id_one',
+        firstName: 'sandun',
+        lastName: 'Tharuka',
+        email: 'sandun@gmail.com',
+      };
+      jest.spyOn(prismaService.customer, 'update').mockResolvedValue(customer);
+
+      expect(await service.update('id_one', customer)).toBe(customer);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a customer', async () => {
+      const customer: Customer = {
+        id: 'id_one',
+        firstName: 'sandun',
+        lastName: 'Tharuka',
+        email: 'sandun@gmail.com',
+      };
+      jest.spyOn(prismaService.customer, 'delete').mockResolvedValue(customer);
+
+      expect(await service.remove('id_one')).toBe(customer);
+    });
   });
 });
