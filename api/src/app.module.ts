7208469347e8ec97503060f4fb95195bf55a6587
Sandun Tharuka <sandun.suntws@gmail.com>
2024-07-23 12:16:36 +0530

    add customer module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    CustomerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
