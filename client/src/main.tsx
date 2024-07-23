9067585451a1378bb8c95bdbcd10bac8ed3c27ea
Sandun Tharuka <sandun.suntws@gmail.com>
2024-07-23 13:01:33 +0530

    add tanstack query provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import TanstackProvider from './providers/TanstackProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <TanstackProvider>
     <App />
+    </TanstackProvider>
+    
   </React.StrictMode>,
 )
